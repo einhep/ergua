d9c3cf0bf01c41c2293b32fbddf242af02c4d341
einhep <einhep@gmail.com>
2014-06-27 14:18:07 +0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ergua::Application.config.secret_key_base = '858bae18c1e21974872e4e36190dde5129b2d40b7c643d0c0e006b009211d0dc592b91b90fe949bd0deeed3b9de09d272051d2a5e8eeed101e574f8eac7b2b82'
