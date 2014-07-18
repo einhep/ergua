202b322edc1bea1932d1df2d49f7daff5457af12
einhep <einhep@gmail.com>
2014-07-18 19:55:15 +0800

    9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user,only:[:edit,:update,:index]
+  before_action :correct_user,only:[:edit,:update]
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -10,14 +16,39 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Model Site"
       redirect_to @user
     else
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Update!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def signed_in_user
+    redirect_to signin_url,notice:"Please sign in."unless sign_in?
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user?(@user)
   end
 
 end
